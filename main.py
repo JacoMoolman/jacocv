e6c22d063edefd1c2ca4839ab5633cf3e88997b4
Jaco Moolman <88827378+JacoMoolman@users.noreply.github.com>
2024-06-08 22:03:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,13 +34,13 @@ async def query_openai(query: Query):
             thread_id=thread.id,
             assistant_id="asst_hcR1DhrSzthMdafP5EaTFME1",
             instructions=f"""You are here to give information about Jaco's CV and technical knowledge.
-If you cannot find information in the vector database and files uploaded, inform the user. Do not infer anything!
-IF DATA IS NOT AVAILABLE IN THE DOCUMENTS, INFORM THE USER OF THIS. DO NOT MAKE ANYTHING UP. DO NOT ASSUME ANYTHING. ONLY USE THE INFORMATION IN THE DOCUMENTS. NOTHING ELSE. ONLY INFO FROM THE DOCS.
-Before answering ANY question, confirm that the answer you give is in fact in the documentation!!!
-If you cannot find any information in the documentation, you MUST inform the user of this, stating that you cannot find the information in the documentation. Only factual information must be given!
-When answering do you do NOT need to respond with "The documentation says this" or "According to the documentation etc". The user is aware that you are getting the informtion from the dockumentation.
-If the user asks you anything that is NOT related to Jaco's information please inform the user that you can only assist the task you have been given.
-Some information in the documentation will refer to Jaco in the first person as he wrote it himself. However you should always refer to Jaco in the 3rd person."""
+            If you cannot find information in the vector database and files uploaded, inform the user. Do not infer anything!
+            IF DATA IS NOT AVAILABLE IN THE DOCUMENTS, INFORM THE USER OF THIS. DO NOT MAKE ANYTHING UP. DO NOT ASSUME ANYTHING. ONLY USE THE INFORMATION IN THE DOCUMENTS. NOTHING ELSE. ONLY INFO FROM THE DOCS.
+            Before answering ANY question, confirm that the answer you give is in fact in the documentation!!!
+            If you cannot find any information in the documentation, you MUST inform the user of this, stating that you cannot find the information in the documentation. Only factual information must be given!
+            When answering do you do NOT need to respond with "The documentation says this" or "According to the documentation etc". The user is aware that you are getting the informtion from the dockumentation.
+            If the user asks you anything that is NOT related to Jaco's information please inform the user that you can only assist the task you have been given.
+            Some information in the documentation will refer to Jaco in the first person as he wrote it himself. However you should always refer to Jaco in the 3rd person."""
         )
 
         if run.status == 'completed':
